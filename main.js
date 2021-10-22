c2a415a889e642c5c468568f00706bcbdf3d7437
UpBeatPredator <77713906+hackerboi72@users.noreply.github.com>
2021-10-22 15:32:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,4 +7,43 @@ function start() {
 }
 recognition.onresult = function run(event) { 
     console.log(event) 
-} 
+    content = event.results[0][0].transcript
+    document.getElementById("textbox").innerHTML = content;
+    if (content == "Take my selfie.") {
+
+    speak()
+    }  
+}
+
+function speak() {
+    var synth = window.speechSynthesis; 
+    speak_data = "taking your selfie in 5 seconds"; 
+    var utter = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utter);  
+    Webcam.attach(camera);
+    setTimeout(
+        function delay(){ 
+          takesnapshot(); 
+          save();
+     }, 5000);
+}
+Webcam.set({
+    width: 800,
+    height: 600,
+    image_format: 'png', 
+    png_quality: 1080  
+}); 
+camera = document.getElementById("camera");
+function takesnapshot() {
+    Webcam.snap(
+        function (data_url) {
+            document.getElementById("result").innerHTML = '<img id="selfie_image" src="' + data_url + '">"'
+        }
+    ) 
+}
+function save() {
+    link = document.getElementById("link")
+    image = document.getElementById("selfie_image").src 
+    link.href = image 
+    link.click()
+}
